52782ac1f6807d8f36f066ec113269138bf7cef3
Tahin Rahman <tahin.36@gmail.com>
2020-03-28 11:19:37 +0600

    Add more preview components for primary color

--- src/components/PrimaryColor.js
+++ src/components/PrimaryColor.js
@@ -8,8 +8,9 @@ import './ColorPicker.css';
 
 const colorService = new ColorService();
 
-const PrimaryColor = ({ primaryColor, primaryColorUndoStack, primaryColorRedoStack }) => {
+const PrimaryColor = ({ primaryColor, grey, primaryColorUndoStack, primaryColorRedoStack }) => {
     const primaryColorShades = colorService.getColorShades(primaryColor);
+    const greyShades = colorService.getGreyShades(primaryColor, grey);
 
     return $(ColumnLayout, {
         center: true,
@@ -31,7 +32,38 @@ const PrimaryColor = ({ primaryColor, primaryColorUndoStack, primaryColorRedoSta
                     class: 'button-primary-color', style: {
                         backgroundColor: primaryColorShades[3]
                     }
-                }, 'Primary Button')
+                }, 'Primary Button'),
+                $('div', {
+                    class: 'alert-primary-color', style: {
+                        borderColor: primaryColorShades[7],
+                        backgroundColor: primaryColorShades[8]
+                    }
+                }, [
+                    $('p', { class: 'alert-title', style: { color: primaryColorShades[0] } }, 'Our privacy policy has changed'),
+                    $('p', { class: 'alert-body', style: { color: primaryColorShades[0] } }, 'Make sure you know how these changes affect you.')
+                ]),
+                $('form', {
+                    class: 'form', style: {
+                        backgroundColor: greyShades[8],
+                        borderColor: greyShades[6]
+                    }
+                }, [
+                    $('h2', { style: { color: greyShades[0] } }, 'Personal information'),
+                    $('p', { style: { color: greyShades[2] } }, 'This is your personal information. This information will be shown publicly.'),
+                    $('h3', { style: { color: greyShades[0] } }, 'First Name'),
+                    $('input', { type: 'text', style: { borderColor: greyShades[6] } }),
+                    $('h3', { style: { color: greyShades[0] } }, 'Last Name'),
+                    $('input', { type: 'text', style: { borderColor: greyShades[6] } }),
+                    $('h3', { style: { color: greyShades[0] } }, 'Email'),
+                    $('input', { type: 'email', style: { borderColor: greyShades[6] } }),
+                    $('div', { class: 'form-buttons' }, [
+                        $('button', {
+                            type: 'submit', class: 'button-primary-color', style: {
+                                backgroundColor: primaryColorShades[3]
+                            }
+                        }, 'Submit')
+                    ])
+                ])
             ])
         ]
     });
